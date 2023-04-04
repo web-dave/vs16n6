7e62cc354f6ed6ad7c3d7be1ce95b2a3f4b528f8
web-dave <dave@webdave.de>
2023-04-04 22:12:28 +0200

    signals

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject, Input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { IUser, UserService } from './user.service';
@@ -9,12 +15,21 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterLink],
   template: `
-    <ng-container *ngFor="let user of users">
+    <ng-container *ngFor="let user of users()">
       <a [routerLink]="[user.id]">{{ user.name }}</a> |
     </ng-container>
+
+    <pre>{{ usersS() | json }}</pre>
   `,
   styleUrls: [],
 })
 export class UsersComponent {
-  users: IUser[] = inject(UserService).users;
+  users: WritableSignal<IUser[]> = signal(inject(UserService).users);
+  usersS = computed(() => this.users().map((u) => u.age));
+
+  constructor() {
+    setTimeout(() => {
+      this.users.mutate((value) => (value[2].age = 99));
+    }, 3000);
+  }
 }
